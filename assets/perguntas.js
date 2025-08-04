9fb926fb9e33c1178a31112a0ac465d14b361ba4
EmaanuellySilvaa <socoloski.emanuelly@escola.pr.gov.br>
2025-08-04 11:55:22 -0300

    app.js

---
+++ assets/perguntas.js
@@ -0,0 +1,26 @@
+Criar o arquivo perguntas.js   e    app.js
+
+no arquivo perguntas.js inserir os comandos:
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+    criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+)
